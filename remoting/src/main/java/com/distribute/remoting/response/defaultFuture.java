cbbf51415b10c98ddf92ee054c27df1b8886d570
smmdwa <1016342179@qq.com>
2021-10-21 20:50:29 +0800

    fix future

--- remoting/src/main/java/com/distribute/remoting/response/defaultFuture.java
+++ remoting/src/main/java/com/distribute/remoting/response/defaultFuture.java
@@ -11,14 +11,20 @@ public class defaultFuture {
     public ResponseMessage getResponse(long timeout) {
         long begin = System.currentTimeMillis();
         //等待间隔，每隔一段时间就查看isSucceed是否为true
-        long waitBeat=timeout/10;
+//        long waitBeat=timeout/10;
         synchronized (object) {
-            while (!isSucceed&& (timeout - (System.currentTimeMillis() - begin))>0) {
-                try {
-                    object.wait(waitBeat);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+            //todo 是否要轮询呢，dubbo里是轮询了的，但是我感觉不需要？
+//            while (!isSucceed) {
+//                try {
+//                    object.wait(timeout);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+            try {
+                object.wait(timeout);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
             return response;
         }
