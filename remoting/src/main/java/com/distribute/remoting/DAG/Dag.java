7b719ff8ea3074d63258abcbe9289c37a5dee16a
smmdwa <1016342179@qq.com>
2022-02-19 11:17:50 +0800

    修改dag图的生成方式-计算入度和出度

--- remoting/src/main/java/com/distribute/remoting/DAG/Dag.java
+++ remoting/src/main/java/com/distribute/remoting/DAG/Dag.java
@@ -14,31 +14,14 @@ public class Dag {
     // 顶点数据
     @Data
     private static class VertexType {
+//        Long jobId;
         NodeDAG node;
         AdjacencyType firstIn = null;// 入边表（顶点为弧尾）
         AdjacencyType firstOut = null;// 出边表（顶点为弧头）
-
-        public VertexType(NodeDAG node) {
-            this.node = node;
+        public VertexType(NodeDAG node){
+            this.node=node;
         }
     }
-    // 顶点数组赋值
-    public Dag(List<NodeDAG> nodes) {
-        this.mavVex = nodes.size();
-        this.verArr = new VertexType[this.mavVex];
-        for (int i = 0; i < nodes.size(); i++) {
-            this.verArr[i] = new VertexType(nodes.get(i));
-        }
-    }
-
-    public void getRelatedDAG(Long jobId) {
-        int position = getPosition(jobId);
-        this.result = new HashSet<>();
-        getDAG(position);
-        fillNodesLevel();
-    }
-
-    HashSet<Integer> result;
 
     @Data
     private static class AdjacencyType {
@@ -49,30 +32,50 @@ public class Dag {
 
     }
 
-    public HashSet<NodeDAG> transferToNodeDag() {
-        HashSet<NodeDAG> res = new HashSet<>();
+    // 顶点数组赋值
+    public Dag(List<NodeDAG>nodes) {
+        this.mavVex = nodes.size();
+        this.verArr = new VertexType[this.mavVex];
+        for (int i = 0; i < nodes.size(); i++) {
+            this.verArr[i] = new VertexType(nodes.get(i));
+        }
+    }
+
+    public void getRelatedDAG(Long jobId){
+        int position = getPosition(jobId);
+        this.result=new HashSet<>();
+        getDAG(position);
+        fillNodesLevel();
+    }
+
+    HashSet<Integer>result;
+
+    public HashSet<NodeDAG> transferToNodeDag(){
+        HashSet<NodeDAG>res=new HashSet<>();
         for (Integer integer : result) {
             res.add(verArr[integer].getNode());
         }
         return res;
     }
 
-    public void getDAG(int pos) {
-        List<Integer> toBeSearch = new ArrayList<>();
+    public void getDAG(int pos){
+        List<Integer>toBeSearch=new ArrayList<>();
         result.add(pos);
         AdjacencyType fristin = this.verArr[pos].getFirstIn();
         AdjacencyType fristout = this.verArr[pos].getFirstOut();
-        while (fristin != null) {
-            if (!result.contains(fristin.getFrom())) {
+        while (fristin!=null){
+            if(!result.contains(fristin.getFrom())){
+//                System.out.println("in:"+fristin.getFrom());
                 toBeSearch.add(fristin.getFrom());
             }
-            fristin = fristin.getHeadLink();
+            fristin=fristin.getHeadLink();
         }
-        while (fristout != null) {
-            if (!result.contains(fristout.getTo())) {
+        while (fristout!=null){
+            if(!result.contains(fristout.getTo())){
+//                System.out.println("out:"+fristout.getTo());
                 toBeSearch.add(fristout.getTo());
             }
-            fristout = fristout.getAdjLink();
+            fristout=fristout.getAdjLink();
         }
 
         for (Integer beSearch : toBeSearch) {
@@ -80,7 +83,7 @@ public class Dag {
         }
     }
 
-    public void addAdj(Long cur, Long next) {
+    public void addAdj(Long cur,Long next) {
         AdjacencyType temp = null;
         AdjacencyType tempin = null;
         int posOne = getPosition(cur);
@@ -119,9 +122,9 @@ public class Dag {
 
     private int getPosition(Long jobId) {
         int pos = 0;
-        for (int i = 0; i < this.mavVex; i++) {
-            if (this.verArr[i].getNode().getJobId().equals(jobId)) {
-                pos = i;
+        for(int i=0;i<this.mavVex;i++){
+            if(this.verArr[i].getNode().getJobId().equals(jobId)) {
+                pos=i;
                 break;
             }
         }
@@ -130,38 +133,71 @@ public class Dag {
 
     //填充node大致的level和belong，为什么是大致，因为DAG不是层级结构，为了前端展示，所以制造大致的层级
     //找到所有入度为0的节点，
-    private void fillNodesLevel() {
-        int belong = 1;
+    private void fillNodesLevel(){
+        int belong=1;
         for (Integer integer : this.result) {
             NodeDAG node = verArr[integer].getNode();
-            if (verArr[integer].getFirstIn() != null)
+            if(verArr[integer].getFirstIn()!=null)
                 continue;
             //找到入度为0的节点开始
-            fill(integer, 1, belong);
+            fill(integer,1,belong);
             belong++;
         }
     }
 
     //进行实际的填充
-    private void fill(Integer pos, int level, int belong) {
+    private void fill(Integer pos,int level,int belong){
         NodeDAG node = verArr[pos].getNode();
         //已经被设置了就跳过
-        if (node.getLevel() != 0) return;
+        if(node.getLevel()!=0)return;
         node.setLevel(level);
         node.setBelong(belong);
         AdjacencyType adjLink = verArr[pos].getFirstOut();
-        while (adjLink != null) {
-            fill(adjLink.getTo(), level + 1, belong);
-            adjLink = adjLink.getAdjLink();
+        while (adjLink!=null){
+            fill(adjLink.getTo(),level+1,belong);
+            adjLink=adjLink.getAdjLink();
         }
     }
 
     //清除所有的level和belong
-    private void clear() {
+    private void clear(){
         for (Integer integer : result) {
             verArr[integer].getNode().setLevel(0);
             verArr[integer].getNode().setBelong(0);
         }
     }
 
+    public int getIndegree(String str) {
+        int i = 0;
+        int pos=getPosition(Long.valueOf(str));
+        AdjacencyType temp = this.verArr[pos].firstIn;
+        if (temp == null)
+            return i;
+        while (true) {
+            if (temp != null) {
+                i++;
+                temp = temp.getHeadLink();
+            } else {
+                break;
+            }
+        }
+        return i;
+    }
+
+    public int getOutdegree(String str) {
+        int i = 0;
+        int pos=getPosition(Long.valueOf(str));
+        AdjacencyType temp = this.verArr[pos].firstOut;
+        if (temp == null)
+            return i;
+        while (true) {
+            if (temp != null) {
+                i++;
+                temp = temp.getAdjLink();
+            } else {
+                break;
+            }
+        }
+        return i;
+    }
 }
